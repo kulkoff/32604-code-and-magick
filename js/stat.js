e8c62826913e2ea0a26f70a19357b1e787f47cf1
Alex Kulkoff <alex.kulkoff@gmail.com>
2017-11-22 16:44:39 +0500

    last changes

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,11 @@
 'use strict';
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.shadowOffsetX = 10;
   ctx.shadowOffsetY = 10;
   ctx.shadowBlur = 2;
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+
   ctx.beginPath();
   ctx.moveTo(310, 10);
   ctx.quadraticCurveTo(40, -10, 100, 145);
@@ -43,11 +45,10 @@ window.renderStatistics = function (ctx, names, times) {
   for (var j = 0; j < times.length; j++) {
     if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(initialX + (indent + barWidth) * j, initialY, barWidth, -times[j] * step);
     } else {
       ctx.fillStyle = 'rgba(51, 153, 255, ' + Math.random() + ')';
-      ctx.fillRect(initialX + (indent + barWidth) * j, initialY, barWidth, -times[j] * step);
     }
+    ctx.fillRect(initialX + (indent + barWidth) * j, initialY, barWidth, -times[j] * step);
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
     ctx.fillText(Math.floor(times[j]), initialX + (indent + barWidth) * j, initialY - lineHeight + -times[j] * step);
